8ac7a9011dde6a7daaff9b7596a6ac5e81c33556
Michiel Meertens <meertensmichiel28@gmail.com>
2015-10-17 00:36:19 +0200

    Update
    Commented examples and added README.md

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-<?php
+# DPD Webservice
 
-	require_once 'vendor/autoload.php';
-	
+Features:
+
+ * Submit a shipment to the dpd webservice and retrieve it's label and tracking information
+ * Retrieve parcel status information
+
+Basic shipment usage:
+
+```php
 use MCS\DPDAuthorisation;
 use MCS\DPDShipment;
 	
@@ -98,6 +104,37 @@ try{
 }catch(Exception $e){
     echo $e->getMessage();		
 }
+```
 
+Basic parcel status usage:
 
+```php
+use MCS\DPDAuthorisation;
+use MCS\DPDParcelStatus;
 
+try{
+
+    // Authorize
+    // Be aware that this functionality doesn't work with test credentials
+    $authorisation = new DPDAuthorisation([
+        'staging' => false,
+        'delisId' => '<delisId>',
+        'password' => '<password>',
+        'messageLanguage' => 'en_EN',
+        'customerNumber' => '<customerNumber>'
+    ]);
+
+    // Init
+    $status = new DPDParcelStatus($authorisation);
+
+    // Retrieve the parcel's status by it's awb number
+    $parcelStatus = $status->getStatus('12345678987654');
+
+    echo '<pre>';
+    print_r($parcelStatus);
+    echo '</pre>';
+
+}catch(Exception $e){
+    echo $e->getMessage();		
+}
+```
--- testParcelStatus.php
+++ testParcelStatus.php
@@ -2,31 +2,34 @@
 
 	require_once 'vendor/autoload.php';
 	
-    use MCS\DPDAuthorisation;
-	use MCS\DPDParcelStatus;
-	
-	try{
-	       
-        // Be aware that this functionality doesn't work with test credentials
-        $authorisation = new DPDAuthorisation([
-            'staging' => false,
-            'delisId' => '<delisId>',
-            'password' => '<password>',
-            'messageLanguage' => 'en_EN',
-            'customerNumber' => '<customerNumber>'
-        ]);
+use MCS\DPDAuthorisation;
+use MCS\DPDParcelStatus;
 
-        $status = new DPDParcelStatus($authorisation);
+try{
 
-        $parcelStatus = $status->getStatus('12345678987654');
-        
-        echo '<pre>';
-        print_r($parcelStatus);
-        echo '</pre>';
-	
-	}catch(Exception $e){
-		echo $e->getMessage();		
-	}
+    // Authorize
+    // Be aware that this functionality doesn't work with test credentials
+    $authorisation = new DPDAuthorisation([
+        'staging' => false,
+        'delisId' => '<delisId>',
+        'password' => '<password>',
+        'messageLanguage' => 'en_EN',
+        'customerNumber' => '<customerNumber>'
+    ]);
+
+    // Init
+    $status = new DPDParcelStatus($authorisation);
+
+    // Retrieve the parcel's status by it's awb number
+    $parcelStatus = $status->getStatus('12345678987654');
+
+    echo '<pre>';
+    print_r($parcelStatus);
+    echo '</pre>';
+
+}catch(Exception $e){
+    echo $e->getMessage();		
+}
 	
 	
 	
